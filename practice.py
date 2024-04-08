f8237378c197e2fe9a073dfc2a5a3a9a457ea694
yogitha <yogithachikka1821@gmail.com>
2024-04-08 21:28:32 +0530

    Change

--- practice.py
+++ practice.py
@@ -1,3 +1,16 @@
+#  Write a program that polls users about their dream vacation.
+# Write a prompt similar to If you could visit one place in the world, where
+# would you go? Include a block of code that prints the results of the poll
+
+# result_of_poll = {}
+# should_continue = False
+# while not should_continue:
+#     place = input("If you could visit one place in the world, where would you go?\n")
+#     result_of_poll[place] = 0
+#     if 
+
+
+
 
 
 
